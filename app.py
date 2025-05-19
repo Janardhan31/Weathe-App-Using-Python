1fc781cf664f1025ba1491b550afa55abfc61939
Janardhan Kumar <janardhankr2003@gmail.com>
2025-05-19 14:51:15 +0530

    Added URL to fetch API

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 import requests
 
 api_key = '30d4741c779ba94c470ca1f63045390a'
+
+
+user_input = input("Enter city: ")
+
+weather_data = requests.get(
+    f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=imperial&APPID={api_key}")
